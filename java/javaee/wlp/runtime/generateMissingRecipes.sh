7f7805ebc9876473af4581bad81e602f0e110e86
Joao Mauricio de O. Alves <jmauricio@pobox.com>
2024-03-05 20:00:04 -0300

    wlp - generateMissingRecipes.sh

---
+++ java/javaee/wlp/runtime/generateMissingRecipes.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+scriptPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd && cd - >/dev/null 2>&1 )"
+
+template=${scriptPath}/wlp-runtime-template.yaml
+for version in $( \
+    curl -kLs https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/wasdev/downloads/wlp/ | grep -o 'href=".*/">' | sed 's/.*href="\(.*\)\/".*/\1/g' | grep '^[0-9]' \
+); do
+    my_arr=($( echo ${version} | tr "." "\n" ))
+    year=${my_arr[0]}
+    unset my_arr
+
+    if [ "${year}" == "8" ]; then
+        # Ignore wlp 8.5.*
+        continue
+    fi
+
+    filename=${scriptPath}/${year}/wlp-runtime-${version}.levain.yaml
+    if [ ! -e ${filename} ]; then
+        echo === MISSING ${version} at ${filename}
+        cat ${template} | sed "s/x.x.x.x/${version}/g" > ${filename}
+    fi
+done
